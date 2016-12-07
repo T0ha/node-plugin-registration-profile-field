1cc440941c82df690912259a6f24a2b2bd54c546
Shvein Anton <t0hashvein@gmail.com>
2016-12-07 18:31:58 +0500

    Fixed plugin name in routes and meta

--- library.js
+++ library.js
@@ -8,28 +8,28 @@ plugin.init = function(params, callback) {
 		middleware = params.middleware,
 		controllers = params.controllers;
 		
-	app.get('/admin/registration-question', middleware.admin.buildHeader, renderAdmin);
-	app.get('/api/admin/registration-question', renderAdmin);
+	app.get('/admin/registration-profile-field', middleware.admin.buildHeader, renderAdmin);
+	app.get('/api/admin/registration-profile-field', renderAdmin);
 
 	callback();
 };
 
 plugin.addAdminNavigation = function(header, callback) {
 	header.plugins.push({
-		route: '/registration-question',
+		route: '/registration-profile-field',
 		icon: 'fa-tint',
-		name: 'Registration Question'
+		name: 'Registration Profile Field'
 	});
 
 	callback(null, header);
 };
 
 plugin.addCaptcha = function(params, callback) {
-	var question = meta.config['registration-question:question'];
+	var question = meta.config['registration-profile-field:question'];
 
 	var captcha = {
 		label: 'Registration Question',
-		html: '<div class="well"><strong>' + question + '</strong><br /><input class="form-control" name="registration-question" id="registration-question" /></div>'
+		html: '<div class="well"><strong>' + question + '</strong><br /><input class="form-control" name="registration-profile-field" id="registration-profile-field" /></div>'
 	};
 
 	if (params.templateData.regFormEntry && Array.isArray(params.templateData.regFormEntry)) {
@@ -42,17 +42,17 @@ plugin.addCaptcha = function(params, callback) {
 };
 
 plugin.checkRegister = function(params, callback) {
-	var answer = meta.config['registration-question:answer'];
+	var answer = meta.config['registration-profile-field:answer'];
 
-	if (answer.toLowerCase() !== params.req.body['registration-question'].toLowerCase()) {
-		callback({source: 'registration-question', message: 'wrong-answer'}, params);
+	if (answer.toLowerCase() !== params.req.body['registration-profile-field'].toLowerCase()) {
+		callback({source: 'registration-profile-field', message: 'wrong-answer'}, params);
 	} else {
 		callback(null, params);
 	}
 };
 
 function renderAdmin(req, res, next) {
-	res.render('admin/registration-question', {});
+	res.render('admin/registration-profile-field', {});
 }
 
 module.exports = plugin;
